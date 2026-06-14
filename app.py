758bccef69c8eee1422a30bc60c264356a297061
Camilla <milla_mendes83@hotmail.com>
2026-06-14 20:25:51 -0300

    refactor: update templates for better accessibility and user experience; add database initialization

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from controllers.minicurso_controller import minicurso_blueprint
 
 app = Flask(__name__)
 
+init_db()
+
 # Registra os Controladores (Blueprints) no app principal
 app.register_blueprint(inscrito_blueprint)
 app.register_blueprint(minicurso_blueprint)
 
 if __name__ == '__main__':
-    # Inicializa o banco criando as tabelas se necessário antes de ligar o servidor
-    init_db()
     app.run(debug=True)
     
